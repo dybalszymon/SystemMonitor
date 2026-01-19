ab7eb1b08a91312fd252d580c0d70f838c51c750
dybalszymon <szymondybal23@gmail.com>
2026-01-19 21:10:23 +0100

    Base sensor first version

---
+++ SystemMonitor/Core/BaseSensor.cs
@@ -0,0 +1,17 @@
+﻿namespace SystemMonitor.Core;
+
+public abstract class BaseSensor : ISensor
+{
+    public string Name { get; protected set;}
+    public string DisplayText { get; protected set;}
+    public double BarValue { get; protected set; }
+    
+    public abstract void Update();
+
+    protected BaseSensor(string name, string displayText)
+    {
+        Name = name;
+        DisplayText = displayText;
+        BarValue = 0;
+    }
+}
